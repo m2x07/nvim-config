4df8c096f519e2b914a2986c3bced5a94384cdc6
m2x07 <meet2x07@gmail.com>
2024-02-24 21:49:50 +0530

    fix: incorrect tree-sitter setup treesitter needs to be configured using 'nvim-treesitter.config' module. i left the config in the 'opts' table of the lazy spec for treesitter which would call 'nvim-treesitter'.setup{} which is incorrect. ig? now its all viola

--- lua/m2x07/plugins/treesitter.lua
+++ lua/m2x07/plugins/treesitter.lua
@@ -3,37 +3,39 @@ return {
 	build = ":TSUpdate",
 	lazy = true,
 	event = "BufReadPre",
-	opts = {
-		-- A list of parser names, or "all" (the five listed parsers should always be installed)
-		ensure_installed = {
-			"bash",
-			"c",
-			"cpp",
-			"html",
-			"javascript",
-			"json",
-			"lua",
-			"markdown",
-			"markdown_inline",
-			"python",
-			"query",
-			"regex",
-			"rust",
-			"tsx",
-			"typescript",
-			"vim",
-			"vimdoc",
-		},
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			-- A list of parser names, or "all" (the five listed parsers should always be installed)
+			ensure_installed = {
+				"bash",
+				"c",
+				"cpp",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"regex",
+				"rust",
+				"tsx",
+				"typescript",
+				"vim",
+				"vimdoc",
+			},
 
-		-- Install parsers synchronously (only applied to `ensure_installed`)
-		sync_install = false,
+			-- Install parsers synchronously (only applied to `ensure_installed`)
+			sync_install = false,
 
-		-- Automatically install missing parsers when entering buffer
-		-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-		auto_install = true,
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = false,
-		},
-	},
+			-- Automatically install missing parsers when entering buffer
+			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+			auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+		})
+	end,
 }
