fdc3994af10de3d16675057e338e68f7e248c5ab
m2x07 <meet2x07@gmail.com>
2024-07-10 00:31:08 +0530

    fix: disable format on save

--- lua/m2x07/plugins/formatting.lua
+++ lua/m2x07/plugins/formatting.lua
@@ -14,13 +14,13 @@ return {
   },
   opts = {
     notify_on_error = false,
-    format_on_save = function(bufnr)
-      local disable_filetypes = { c = true, cpp = true }
-      return {
-        timeout_ms = 500,
-        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-      }
-    end,
+    -- format_on_save = function(bufnr)
+    --   local disable_filetypes = { c = true, cpp = true }
+    --   return {
+    --     timeout_ms = 500,
+    --     lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+    --   }
+    -- end,
     formatters_by_ft = {
       -- NOTE: Do not configure formatting for javascript/typescript or any
       -- other project, which involves using node. use the npm package for
