cb938c343ec31457e631237a230078e83fbfb950
m2x07 <meet2x07@gmail.com>
2024-09-14 04:30:31 +0530

    minor changes

--- lua/m2x07/plugins/mini.lua
+++ lua/m2x07/plugins/mini.lua
@@ -1,17 +1,17 @@
 return {
-  'echasnovski/mini.nvim',
-  config = function ()
+  "echasnovski/mini.nvim",
+  config = function()
     -- Better Around/Inside textobject
     -- meaning better a/i movements, like 'va(' or 'ya(' and similar
-    require('mini.ai').setup()
+    require("mini.ai").setup()
 
     -- Add/delete/replace surrounding (brackets, quotes, etc)
-    require('mini.surround').setup()
+    require("mini.surround").setup()
 
     -- Nice animation for movements
     require("mini.animate").setup()
 
     -- Sweet movements with Brackets
-    require('mini.bracketed').setup()
-  end
+    require("mini.bracketed").setup()
+  end,
 }
