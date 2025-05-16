9e0510891167fea5f97573f19b21fc58a70e1297
m2x07 <meet2x07@gmail.com>
2025-05-16 18:42:06 +0530

    fix: update markdown preview plugin spec

--- lua/m2x07/plugins/extra.lua
+++ lua/m2x07/plugins/extra.lua
@@ -84,15 +84,12 @@ return {
   },
   {
     "iamcco/markdown-preview.nvim",
-    cmd = {
-      "MarkdownPreviewToggle",
-      "MarkdownPreview",
-      "MarkdownPreviewStop",
-    },
-    ft = { "markdown" },
-    build = function()
-      vim.fn["mkdp#util#install"]()
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
     end,
+    ft = { "markdown" },
   },
   {
     "MeanderingProgrammer/render-markdown.nvim",
