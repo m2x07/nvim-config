fab470c3b8e239be95b6f4060398ba5e502199a0
m2x07 <meet2x07@gmail.com>
2026-02-23 03:03:44 +0530

    minor changes

--- lua/m2x07/plugins/extra/typescript.lua
+++ lua/m2x07/plugins/extra/typescript.lua
@@ -2,13 +2,20 @@ return {
   "pmizio/typescript-tools.nvim",
   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
   ft = { "javascriptreact", "typescriptreact", "javascript", "typescript" },
-  opts = {
-    -- settings = {
-    --   expose_as_code_action = "all"
-    -- }
-    jsx_close_tag = {
-      enable = true,
-      filetypes = { "javascriptreact", "typescriptreact" },
-    },
-  },
+  config = function()
+    require("typescript-tools").setup {
+      settings = {
+        -- config options to pass to tsserver can be specified here
+        -- https://github.com/microsoft/TypeScript/blob/v5.0.4/src/server/protocol.ts#L3439
+        tsserver_file_preferences = {
+          disableSuggestions = true
+        },
+        expose_as_code_action = "all",
+        jsx_close_tag = {
+          enable = true,
+          filetypes = { "javascriptreact", "typescriptreact" },
+        },
+      }
+    }
+  end
 }
